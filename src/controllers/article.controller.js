61dc2250b9636ac43360d7a0e1811d18153a2a9e
choi-mk <xhrlmin10@naver.com>
2025-05-20 17:33:31 +0900

    feat: article, card layer architecture 적용

---
+++ src/controllers/article.controller.js
@@ -0,0 +1,53 @@
+import express from "express";
+import articleService from "../services/article.service.js";
+
+const articleController = express.Router();
+
+articleController.get("/", async (req, res, next) => {
+  try {
+    const articles = await articleService.getByFilter();
+    res.status(200).json(photocards);
+  } catch (error) {
+    next(error);
+  }
+});
+
+articleController.get("/:id", async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const articles = await articleService.getById(id);
+    res.status(200).json(photocards);
+  } catch (error) {
+    next(error);
+  }
+});
+
+articleController.post("/", async (req, res, next) => {
+  try {
+    const userId = req.auth.userId;
+    const {
+      title,
+      price,
+      totalQuantity,
+      exchangeText,
+      exchangeRank,
+      exchangeGenre,
+      userPhotoCardId,
+    } = req.body;
+    const article = await articleService.postArticle({
+      title,
+      price,
+      totalQuantity,
+      exchangeText,
+      exchangeRank,
+      exchangeGenre,
+      userPhotoCardId,
+      userId,
+    });
+    res.status(201).json(article);
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default articleController;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,11 @@
 import express from "express";
+import cardController from "../controllers/card.controller.js";
+import articleController from "../controllers/article.controller.js";
 
 const router = express.Router();
+
+router.use("/cards", cardController);
+router.use("/articles", articleController);
 
 export default router;
 //수정용

6fa89092e30930bf024a6dbe2857839e594c75de
rlawodnr1109 <wodnr1109@naver.com>
2025-05-21 13:32:35 +0900

    fix: 변수명 변경

--- src/services/notificationService.js
+++ src/services/notificationService.js
@@ -2,39 +2,39 @@ import notificationRepository from "../repositories/notificationRepository";
 
 const notificationService = {
   // 포토 카드 교환 성사 알림 생성 (구매자 대상)
-  createExchangeSuccessNotification: async (buyerUserId, counterpartNickname, grade, name) => {
-    const message = `${counterpartNickname} 님과의 [${grade} | ${name}] 포토카드 교환이 성사되었습니다.`;
+  createExchangeSuccessNotification: async (buyerUserId, counterpartNickname, rank, title) => {
+    const message = `${counterpartNickname} 님과의 [${rank} | ${title}] 포토카드 교환이 성사되었습니다.`;
     await notificationRepository.createNotification(message, [buyerUserId]);
   },
 
   // 포토 카드 교환 불발 알림 생성 (구매자 대상)
-  createExchangeFailureNotification: async (buyerUserId, counterpartNickname, grade, name) => {
-    const message = `${counterpartNickname} 님과의 [${grade} | ${name}] 포토카드 교환이 불발되었습니다.`;
+  createExchangeFailureNotification: async (buyerUserId, counterpartNickname, rank, title) => {
+    const message = `${counterpartNickname} 님과의 [${rank} | ${title}] 포토카드 교환이 불발되었습니다.`;
     await notificationRepository.createNotification(message, [buyerUserId]);
   },
 
   // ?? 포토 카드 구매 성사 알림 생성 (구매자 대상)
-  createPurchaseSuccessNotification: async (buyerUserId, grade, name, quantity) => {
-    const message = `[${grade} | ${name}] ${quantity}장을 성공적으로 구매했습니다.`;
+  createPurchaseSuccessNotification: async (buyerUserId, rank, title, quantity) => {
+    const message = `[${rank} | ${title}] ${quantity}장을 성공적으로 구매했습니다.`;
     await notificationRepository.createNotification(message, [buyerUserId]);
   },
 
   // 포토 카드 교환 제안 알림 생성 (판매자 대상)
-  createExchangeOfferNotification: async (sellerUserId, proposingUserNickname, grade, name) => {
-    const message = `${proposingUserNickname} 님이 [${grade} | ${name}] 포토카드 교환을 제안했습니다.`;
+  createExchangeOfferNotification: async (sellerUserId, proposingUserNickname, rank, title) => {
+    const message = `${proposingUserNickname} 님이 [${rank} | ${title}] 포토카드 교환을 제안했습니다.`;
     await notificationRepository.createNotification(message, [sellerUserId]);
   },
 
   // 포토 카드 판매 성사 알림 생성 (판매자 대상)
-  createSaleSuccessNotification: async (sellerUserId, grade, name, quantity) => {
-    const message = `[${grade} | ${name}] ${quantity}장을 성공적으로 판매했습니다.`;
+  createSaleSuccessNotification: async (sellerUserId, rank, title, quantity) => {
+    const message = `[${rank} | ${title}] ${quantity}장을 성공적으로 판매했습니다.`;
     await notificationRepository.createNotification(message, [sellerUserId]);
   },
 
   // 포토 카드 품절 알림 생성 (관심 목록 등록 유저들 또는 판매자 대상). 여러명에게
-  createSoldOutNotification: async (targetUserIds, grade, name) => {
-    const message = `[${grade} | ${name}] 이/가 품절 되었습니다.`;
-    await notificationRepository.createNotification(message, targetUserIds);
+  createSoldOutNotification: async (buyerUserIds, rank, title) => {
+    const message = `[${rank} | ${title}] 이/가 품절 되었습니다.`;
+    await notificationRepository.createNotification(message, buyerUserIds);
   },
 
   // 알림 조회

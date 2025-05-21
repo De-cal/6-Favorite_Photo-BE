32d330ba84650d443357d2d8974cc697f5940861
choi-mk <xhrlmin10@naver.com>
2025-05-21 17:54:06 +0900

    fix:주석 해제

--- src/routes/pointRouter.js
+++ src/routes/pointRouter.js
@@ -4,6 +4,6 @@ import pointController from "../controllers/pointController.js";
 const pointRouter = express.Router();
 
 // 포인트 추가.
-// pointRouter.post("/", auth.verifyAccessToken, pointController.addPoint);
+pointRouter.post("/", auth.verifyAccessToken, pointController.addPoint);
 
 export default pointRouter;

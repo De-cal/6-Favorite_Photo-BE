bbff6f1ae8718c297c07c4e8b714aa94788488f5
rlawodnr1109 <wodnr1109@naver.com>
2025-05-21 13:08:14 +0900

    feat: 알림 기능 MVC 패턴으로 구현.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import pointRouter from "./pointRouter";
+import notificationRouter from "./notificationRouter";
 
 const router = express.Router();
 
 router.use("/point", pointRouter)
+router.use("/notification", notificationRouter)
 
 export default router;

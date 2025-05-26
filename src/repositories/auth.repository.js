78c1c5aeca4ba386e9a2d10d0317636f9c1e5890
Jam1eL1 <23jisu@gmail.com>
2025-05-26 18:05:33 +0900

    feat: auth.repository & auth.route 생성

---
+++ src/repositories/auth.repository.js
@@ -0,0 +1,38 @@
+import { User } from "../models/User.js";
+import { prisma } from "../db/prisma/index.js";
+
+export const authRepository = {
+  async findByEmail(email) {
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+    return user ? new User(user) : null;
+  },
+
+  async findByNickname(nickname) {
+    const user = await prisma.user.findUnique({
+      where: { nickname },
+    });
+    return user ? new User(user) : null;
+  },
+
+  async create({ email, nickname, password }) {
+    const user = await prisma.user.create({
+      data: {
+        email,
+        nickname,
+        password,
+        pointAmount: 0,
+        createCount: 0,
+      },
+    });
+    return new User(user);
+  },
+
+  async findById(id) {
+    const user = await prisma.user.findUnique({
+      where: { id },
+    });
+    return user ? new User(user) : null;
+  },
+};

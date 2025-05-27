82bcf33e721e5487b1a034b96d9899b70bfa40e3
Jam1eL1 <23jisu@gmail.com>
2025-05-27 19:03:25 +0900

    refactor: 팀내 에러 컨벤션에 맞게 로직 수정하여 개선

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -30,17 +30,23 @@ const generateTokens = (user) => {
 
 const signup = async ({ email, nickname, password, passwordConfirmation }) => {
   if (password !== passwordConfirmation) {
-    throw new Error("비밀번호가 일치하지 않습니다.");
+    const error = new Error("비밀번호가 일치하지 않습니다.");
+    error.code = 400;
+    throw error;
   }
 
   const existingUserByEmail = await authRepository.findByEmail(email);
   if (existingUserByEmail) {
-    throw new Error("Email already exists");
+    const error = new Error("이미 존재하는 이메일 입니다.");
+    error.code = 409; // Conflict
+    throw error;
   }
 
   const existingUserByNickname = await authRepository.findByNickname(nickname);
   if (existingUserByNickname) {
-    throw new Error("Nickname already exists");
+    const error = new Error("이미 존재하는 닉네임 입니다.");
+    error.code = 409; // Conflict
+    throw error;
   }
 
   const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
@@ -67,12 +73,16 @@ const signup = async ({ email, nickname, password, passwordConfirmation }) => {
 const login = async ({ email, password }) => {
   const user = await authRepository.findByEmail(email);
   if (!user) {
-    throw new Error("올바르지 않은 이메일이거나 패스워드입니다.");
+    const error = new Error("올바르지 않은 이메일이거나 패스워드입니다.");
+    error.code = 401;
+    throw error;
   }
 
   const isPasswordValid = await bcrypt.compare(password, user.password);
   if (!isPasswordValid) {
-    throw new Error("비밀번호가 일치하지 않습니다.");
+    const error = new Error("비밀번호가 일치하지 않습니다.");
+    error.code = 401;
+    throw error;
   }
 
   const { accessToken, refreshToken } = generateTokens(user);
